92a7339db85ec64f8e088169b4d38c1aea7aa93d
Petar Kirov <petar.p.kirov@gmail.com>
2023-10-26 12:07:29 +0300

    refactor(packages): Use flake-parts style module

--- packages/default.nix
+++ packages/default.nix
@@ -1,12 +1,12 @@
-{
-  pkgs,
-  inputs',
-  ...
-}: let
-  lido-withdrawals-automation = pkgs.callPackage ./lido-withdrawals-automation {};
-in {
-  packages = {
-    inherit lido-withdrawals-automation;
-    validator-ejector = inputs'.validator-ejector.packages.validator-ejector;
+{...}: {
+  perSystem = {
+    inputs',
+    pkgs,
+    ...
+  }: {
+    packages = {
+      lido-withdrawals-automation = pkgs.callPackage ./lido-withdrawals-automation {};
+      validator-ejector = inputs'.validator-ejector.packages.validator-ejector;
+    };
   };
 }
