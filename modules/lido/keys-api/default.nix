eaacdd3c77062b4becbace3b5ed185eaaaeeb38a
Petar Kirov <petar.p.kirov@gmail.com>
2023-10-27 17:41:32 +0300

    feat(modules/keys-api): Add postgresql container to make the module self-contained

--- modules/lido/keys-api/default.nix
+++ modules/lido/keys-api/default.nix
@@ -6,10 +6,6 @@
     ...
   }: let
     cfg = config.services.lido-keys-api;
-    package = withSystem pkgs.stdenv.hostPlatform.system (
-      {config, ...}:
-        config.packages.lido-keys-api
-    );
     inherit (import ../../lib.nix {inherit lib;}) toEnvVariables;
   in {
     options.services.lido-keys-api = with lib; {
@@ -164,54 +160,29 @@
         };
       };
     };
+
     config = {
       virtualisation.oci-containers = lib.mkIf cfg.enable {
         backend = "docker";
-        containers.lido-keys-api = {
-          image = "lidofinance/lido-keys-api:stable";
-          environment = toEnvVariables cfg.args;
+        containers = {
+          lido-keys-api = {
+            image = "lidofinance/lido-keys-api:stable";
+            environment = toEnvVariables cfg.args;
+            ports = ["${toString cfg.args.port}:${toString cfg.args.port}"];
+            dependsOn = ["postgresql-lido"];
+          };
+
+          postgresql-lido = {
+            image = "postgres:16-alpine";
+            environment = {
+              POSTGRES_DB = "${cfg.args.db-name}";
+              POSTGRES_USER = "${cfg.args.db-user}";
+              POSTGRES_PASSWORD = "${cfg.args.db-password}";
+            };
+            ports = ["${toString cfg.args.db-port}:${toString cfg.args.db-port}"];
+          };
         };
       };
-
-      #   systemd.services.lido-keys-api = lib.mkIf cfg.enable {
-      #     description = "Lido Keys API";
-
-      #     wantedBy = ["multi-user.target"];
-
-      #     environment =
-      #       lib.filterAttrs
-      #       (k: v: (v != "null") && (v != "") && (v != null))
-      #       {
-      #         PORT = cfg.args.port;
-      #         CORS_WHITELIST_REGEXP = cfg.args.cors-whitelist-regexp;
-      #         GLOBAL_THROTTLE_TTL = cfg.args.global-throttle-ttl;
-      #         GLOBAL_THROTTLE_LIMIT = cfg.args.global-trottle-limit;
-      #         GLOBAL_CACHE_TTL = cfg.args.global-cache-ttl;
-      #         SENTRY_DSN = cfg.args.sentry-dsn;
-      #         LOG_LEVEL = cfg.args.log-level;
-      #         LOG_FORMAT = cfg.args.log-format;
-      #         PROVIDERS_URLS = cfg.args.providers-urls;
-      #         CHRONIX_PROVIDER_MAINNET_URL = cfg.args.chronix-provider-mainnet-url;
-      #         CHAIN_ID = cfg.args.chain-id;
-      #         DB_NAME = cfg.args.db-name;
-      #         DB_PORT = cfg.args.db-port;
-      #         DB_HOST = cfg.args.db-host;
-      #         DB_USER = cfg.args.db-user;
-      #         DB_PASSWORD = cfg.args.db-password;
-      #         PROVIDER_JSON_RPC_MAX_BATCH_SIZE = cfg.args.provider-json-rpc-max-batch-size;
-      #         PROVIDER_CONCURRENT_REQUESTS = cfg.args.provider-concurrent-requests;
-      #         PROVIDER_BATCH_AGGREGATION_WAIT_MS = cfg.args.provider-batch-aggregation-wait-ms;
-      #         CL_API_URLS = cfg.args.cl-api-urls;
-      #         VALIDATOR_REGISTRY_ENABLE = cfg.args.validator-registry-enable;
-      #       };
-
-      #     path = [package];
-
-      #     serviceConfig = {
-      #       ExecStart = "asd";
-      #       WorkingDirectory = "asdf";
-      #     };
-      #   };
     };
   };
 }
