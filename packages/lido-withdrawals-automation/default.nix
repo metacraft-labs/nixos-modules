18ab40b4a58065c17a58275a88aecff966ba4315
Martin Nikov <mnikov12@gmail.com>
2023-12-06 17:29:07 +0200

    config(packages): Update `lido-withdrawals-automation` package

--- packages/lido-withdrawals-automation/default.nix
+++ packages/lido-withdrawals-automation/default.nix
@@ -7,18 +7,11 @@ buildNpmPackage rec {
   pname = "lido-withdrawals-automation";
   version = "1.0.2";
 
-  # src = fetchFromGitHub {
-  #   owner = "status-im";
-  #   repo = "lido-withdrawals-automation";
-  #   rev = "v${version}";
-  #   hash = "sha256-XW+IpB+KHYXf0tm90bztjuM/tYRM1/uzZ9rwg50gAcU=";
-  # };
-
   src = fetchFromGitHub {
-    owner = "MartinNikov";
+    owner = "status-im";
     repo = "lido-withdrawals-automation";
-    rev = "27a5fb57e29b004891d1717d94c6585dc0cef4f6";
-    hash = "sha256-9Uj/o2MQwj0EL6h8FtlCLl+HrNrTiPWpr998syqwEgk=";
+    rev = "v${version}";
+    hash = "sha256-R7G4YtbWVpQS47FOg8juQ6yPMbxN3JYOwPSHzxZx210=";
   };
 
   npmDepsHash = "sha256-cR9smnQtOnpY389ay54SdbR5qsD2MD6zB2X43tfoHwM=";
