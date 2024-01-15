6f86b5f034a22cf25dc98cc7ae66fcb8aa1a4c49
Petar Kirov <petar.p.kirov@gmail.com>
2024-01-15 14:43:08 +0200

    build(lib/current-flake.nix): Fix stale reference to old flake input

--- lib/current-flake.nix
+++ lib/current-flake.nix
@@ -1,6 +1,6 @@
 let
   currentFlake = builtins.fromJSON (builtins.readFile ../flake.lock);
-  inherit (currentFlake.nodes.nixos-2305.locked) owner repo rev narHash;
+  inherit (currentFlake.nodes.nixos-2311.locked) owner repo rev narHash;
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
     sha256 = narHash;
