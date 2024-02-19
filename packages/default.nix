fc09c2ef1951c7fc0afacbf270044ff203c91efe
monyarm <monyarm@gmail.com>
2024-02-19 14:33:13 +0200

    fix(packages): only build validator-ejector on linux

--- packages/default.nix
+++ packages/default.nix
@@ -4,13 +4,16 @@
     pkgs,
     ...
   }: rec {
-    packages = {
-      lido-withdrawals-automation = pkgs.callPackage ./lido-withdrawals-automation {};
-      pyroscope = pkgs.callPackage ./pyroscope {};
-      grafana-agent = import ./grafana-agent {inherit inputs';};
-      validator-ejector = inputs'.validator-ejector.packages.validator-ejector;
-      ci-matrix = pkgs.callPackage ./ci-matrix {};
-    };
+    packages =
+      rec {
+        lido-withdrawals-automation = pkgs.callPackage ./lido-withdrawals-automation {};
+        pyroscope = pkgs.callPackage ./pyroscope {};
+        grafana-agent = import ./grafana-agent {inherit inputs';};
+        ci-matrix = pkgs.callPackage ./ci-matrix {};
+      }
+      // pkgs.lib.optionalAttrs pkgs.hostPlatform.isLinux rec {
+        validator-ejector = inputs'.validator-ejector.packages.validator-ejector;
+      };
     checks = packages;
   };
 }
