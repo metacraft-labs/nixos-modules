dddf8cca0d273693bf1cc1a474f248cf1ed549be
monyarm <monyarm@gmail.com>
2024-02-27 13:16:48 +0200

    build(nix/scripts): improve packaging

--- packages/ci-matrix/default.nix
+++ packages/ci-matrix/default.nix
@@ -1,21 +1,38 @@
-{pkgs}: let
+{
+  pkgs,
+  lib,
+}: let
+  options = {
+    dir = "bin";
+    isExecutable = true;
+
+    jqBin = "${pkgs.jq}/bin/jq";
+    cachixBin = "${pkgs.cachix}/bin/cachix";
+    nixEvalJobsBin = "${pkgs.nix-eval-jobs}/bin/nix-eval-jobs";
+  };
+
+  scripts = rec {
+    system-info = pkgs.substituteAll ({
+        src = ./system-info.sh;
+      }
+      // options);
+    nix-eval-jobs = pkgs.substituteAll ({
+        systemInfoSh = "${system-info}/bin/system-info.sh";
+        src = ./nix-eval-jobs.sh;
+      }
+      // options);
+    ci-matrix = pkgs.substituteAll ({
+        nixEvalJobsSh = "${nix-eval-jobs}/bin/nix-eval-jobs.sh";
+        src = ./ci-matrix.sh;
+      }
+      // options);
+  };
 in
   with pkgs;
-    stdenv.mkDerivation rec {
-      pname = "ci-matrix";
-      version = "main";
+    pkgs.symlinkJoin rec {
+      name = "ci-matrix";
 
-      src = ./.;
-
-      buildPhase = ''
-        sed -i 's|jq|${jq}/bin/jq|' *.sh
-        sed -i 's|nix-eval-jobs |${nix-eval-jobs}/bin/nix-eval-jobs |' *.sh
-        sed -i 's|"$root_dir/scripts/|"'$out'/bin/|' *.sh
-      '';
-      installPhase = ''
-        mkdir -p $out/bin
-        cp {ci-matrix,nix-eval-jobs,system-info}.sh $out/bin'';
-      doCheck = false;
+      paths = lib.attrValues scripts;
 
       meta.mainProgram = "ci-matrix.sh";
     }
