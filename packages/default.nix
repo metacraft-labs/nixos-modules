06621892576846e2c99523320ae25b8e7771cd8a
monyarm <monyarm@gmail.com>
2024-02-29 14:47:09 +0200

    fix(ci-matrix): only build on linux

--- packages/default.nix
+++ packages/default.nix
@@ -11,10 +11,10 @@
         lido-withdrawals-automation = pkgs.callPackage ./lido-withdrawals-automation {};
         pyroscope = pkgs.callPackage ./pyroscope {};
         grafana-agent = import ./grafana-agent {inherit inputs';};
-        ci-matrix = pkgs.callPackage ./ci-matrix {inherit unstablePkgs;};
         deploy-spec = pkgs.callPackage ./deploy-spec {};
       }
       // pkgs.lib.optionalAttrs pkgs.hostPlatform.isLinux rec {
+        ci-matrix = pkgs.callPackage ./ci-matrix {inherit unstablePkgs;};
         validator-ejector = inputs'.validator-ejector.packages.validator-ejector;
       };
     checks = packages;
