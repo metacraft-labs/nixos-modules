c93f02a2ca5d0ea2069622f2d9fbab665482ef76
Petar Kirov <petar.p.kirov@gmail.com>
2024-03-29 05:24:14 +0200

    build(mcl/nix): Package the MCL program

--- packages/default.nix
+++ packages/default.nix
@@ -3,16 +3,24 @@
     inputs',
     pkgs,
     ...
-  }: rec {
+  }: let
+    inherit (pkgs.hostPlatform) isLinux isx86;
+  in rec {
     packages =
-      rec {
+      {
         lido-withdrawals-automation = pkgs.callPackage ./lido-withdrawals-automation {};
         pyroscope = pkgs.callPackage ./pyroscope {};
         grafana-agent = import ./grafana-agent {inherit inputs';};
         ci-matrix = pkgs.callPackage ./ci-matrix {};
       }
-      // pkgs.lib.optionalAttrs pkgs.hostPlatform.isLinux rec {
-        validator-ejector = inputs'.validator-ejector.packages.validator-ejector;
+      // pkgs.lib.optionalAttrs isLinux {
+        inherit (inputs'.validator-ejector.packages) validator-ejector;
+      }
+      // pkgs.lib.optionalAttrs (isLinux && isx86) {
+        mcl = pkgs.callPackage ./mcl {
+          inherit (inputs'.dlang-nix.packages) dub;
+          dcompiler = inputs'.dlang-nix.packages.ldc;
+        };
       };
     checks = packages;
   };
