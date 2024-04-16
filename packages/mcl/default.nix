7f9bc82d17a04943dcc3c17fe01ef36914e7c2b2
monyarm <monyarm@gmail.com>
2024-04-16 15:44:06 +0300

    build(mcl): switch from mkDerivation to buildDubPackage

--- packages/mcl/default.nix
+++ packages/mcl/default.nix
@@ -1,47 +1,28 @@
 {
   lib,
-  stdenv,
-  dub,
-  dcompiler,
+  buildDubPackage,
   unstablePkgs,
   pkgs,
   fetchgit,
   ...
-}:
-stdenv.mkDerivation rec {
-  name = "mcl";
-  src = lib.fileset.toSource {
-    root = ./.;
-    fileset = lib.fileset.fileFilter (file: builtins.any file.hasExt ["d" "sdl" "json"]) ./.;
-  };
+}: let
+  deps = with pkgs; [cachix git nix unstablePkgs.nix-eval-jobs curl];
+in
+  buildDubPackage rec {
+    pname = "mcl";
+    version = "unstable";
+    src = lib.fileset.toSource {
+      root = ./.;
+      fileset = lib.fileset.fileFilter (file: builtins.any file.hasExt ["d" "sdl" "json"]) ./.;
+    };
 
-  nativeBuildInputs = [dub dcompiler pkgs.makeWrapper];
+    nativeBuildInputs = [pkgs.makeWrapper] ++ deps;
+    buildInputs = deps;
+    checkInputs = deps;
+    postFixup = ''
+      wrapProgram $out/bin/${pname} --set PATH ${lib.makeBinPath deps}
+    '';
+    dubTestFlags = ["--" "-e" "(nix\\.(build|eval|run)\\!JSONValue)|(nix\\.(build|eval|run))|fetchJson"];
 
-  silly = fetchgit {
-    url = "https://gitlab.com/AntonMeep/silly";
-    rev = "v1.1.1";
-    sha256 = "sha256-pggc+tlxoiSngmSwOT7euXkbcChwRuicVo/FL20tn3s=";
-  };
-
-  buildPhase = ''
-    mkdir home
-    export HOME="$(pwd)/home"
-    dub add-local "${silly}" "1.1.1"
-    dub build
-  '';
-  checkPhase = "dub test";
-  installPhase = ''
-    install -D -m755 ./build/${name} $out/bin/${name}
-  '';
-  postFixup = ''
-    wrapProgram $out/bin/${name} --set PATH ${lib.makeBinPath [
-      pkgs.cachix
-      pkgs.git
-      pkgs.nix
-      unstablePkgs.nix-eval-jobs
-      pkgs.curl
-    ]}
-  '';
-
-  meta.mainProgram = name;
-}
+    meta.mainProgram = pname;
+  }
