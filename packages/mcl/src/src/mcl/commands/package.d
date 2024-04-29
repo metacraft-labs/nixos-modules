beb41a146c537bb5285f8169aa64348ef6d6d38d
monyarm <monyarm@gmail.com>
2024-04-29 17:31:41 +0300

    feat(mcl/host_info): host_info command

--- packages/mcl/src/src/mcl/commands/package.d
+++ packages/mcl/src/src/mcl/commands/package.d
@@ -5,3 +5,4 @@ public import mcl.commands.deploy_spec : deploy_spec;
 public import mcl.commands.ci_matrix : ci_matrix, print_table;
 public import mcl.commands.shard_matrix : shard_matrix;
 public import mcl.commands.ci : ci;
+public import mcl.commands.host_info : host_info;
