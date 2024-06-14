4eccb5edffd8867bfd7ea0283d73626640b9d9e9
Simeon Armenchev <monyarm@gmail.com>
2024-06-14 15:27:41 +0300

    feat(mcl/utils): Add uniqArrays function

--- packages/mcl/src/src/mcl/utils/array.d
+++ packages/mcl/src/src/mcl/utils/array.d
@@ -1,11 +1,39 @@
 module mcl.utils.array;
-import std.algorithm : all;
+import std.algorithm : all, uniq, sort;
+import std.array : array;
+import std.conv : to;
+import std.traits : isArray, isSomeString;
+import std.stdio : writeln;
 
 T[] uniqIfSame(T)(T[] arr)
 {
-    if (arr.all!(a => a == arr[0])) {
+    if (arr.length == 0)
+    {
+        return arr;
+    }
+    else if (arr.all!(a => a == arr[0])) {
         return [arr[0]];
     }
-    return arr;
+    else {
+        return arr;
+    }
 
+}
+
+T uniqArrays(T)(T s){
+    static if (isSomeString!T){
+        return s;
+    }
+    else static if (isArray!T){
+        return s.sort.uniq.array.to!T;
+    }
+    else static if (is(T == struct)){
+        static foreach (idx, field; T.tupleof){
+            s.tupleof[idx] = s.tupleof[idx].uniqArrays;
+        }
+        return s;
+    }
+    else{
+        return s;
+    }
 }
