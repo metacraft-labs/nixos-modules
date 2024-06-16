2f4c98644cb903d991ef0a0670a3f9e1521ac936
Petar Kirov <petar.p.kirov@gmail.com>
2024-06-16 22:22:45 +0300

    feat(outputs): Expose all the `packages` outputs of most inputs

--- packages/default.nix
+++ packages/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{lib,  ...}: {
   perSystem = {
     inputs',
     pkgs,
@@ -6,6 +6,24 @@
   }: let
     inherit (pkgs.hostPlatform) isLinux isx86;
   in rec {
+    legacyPackages = {
+      inputs = {
+        agenix = inputs'.agenix.packages;
+        cachix = inputs'.cachix.packages;
+        devenv = inputs'.devenv.packages;
+        disko = inputs'.disko.packages;
+        dlang-nix = inputs'.dlang-nix.packages;
+        ethereum-nix = inputs'.ethereum-nix.packages;
+        fenix = inputs'.fenix.packages;
+        git-hooks-nix = inputs'.git-hooks-nix.packages;
+        microvm = inputs'.microvm.packages;
+        nix-fast-build = inputs'.nix-fast-build.packages;
+        nixos-anywhere = inputs'.nixos-anywhere.packages;
+        terranix = inputs'.terranix.packages;
+        treefmt-nix = inputs'.treefmt-nix.packages;
+      };
+    };
+
     packages =
       {
         lido-withdrawals-automation = pkgs.callPackage ./lido-withdrawals-automation {};
