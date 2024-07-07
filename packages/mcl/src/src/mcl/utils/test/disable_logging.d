a7aded977c004535eee22daae54a3c89005adc83
Petar Kirov <petar.p.kirov@gmail.com>
2024-07-07 15:48:57 +0300

    refactor(mcl/utils/test): Make the entire module constructor conditional
    And use selective imports.

--- packages/mcl/src/src/mcl/utils/test/disable_logging.d
+++ packages/mcl/src/src/mcl/utils/test/disable_logging.d
@@ -1,11 +1,10 @@
 module packages.mcl.src.src.mcl.utils.test.unittests;
-import std.logger;
 
-shared static this()
+version (unittest)
 {
-    version (unittest)
+    shared static this()
     {
+        import std.logger : sharedLog, LogLevel, NullLogger;
         sharedLog = cast(shared NullLogger) new NullLogger(LogLevel.all);
     }
-
 }
