cc52333d5bd17fc83c999420fc6f5b80e211bb45
Petar Kirov <petar.p.kirov@gmail.com>
2024-07-17 06:20:11 +0300

    feat(mcl.commands.deploy_spec): Implement `createMachineDeploySpec` and use it

--- packages/mcl/src/src/mcl/commands/deploy_spec.d
+++ packages/mcl/src/src/mcl/commands/deploy_spec.d
@@ -6,20 +6,50 @@ import std.path : buildPath;
 
 import mcl.utils.process : spawnProcessInline;
 import mcl.utils.path : resultDir;
+import mcl.utils.env : parseEnv;
 
-import mcl.commands.ci_matrix : ci_matrix;
+import mcl.commands.ci_matrix : ci_matrix, Params, nixEvalJobs, SupportedSystem;
+
+shared string deploySpecFile;
+
+shared static this()
+{
+    deploySpecFile = resultDir.buildPath("cachix-deploy-spec.json");
+}
 
 export void deploy_spec()
 {
-    auto deploySpecFile = resultDir.buildPath("cachix-deploy-spec.json");
-
     if (!deploySpecFile.exists)
     {
         warningf("'%s' file not found - building...", deploySpecFile);
-        ci_matrix();
+        createMachineDeploySpec();
     }
 
     spawnProcessInline([
         "cachix", "deploy", "activate", deploySpecFile
     ]);
+}
+
+void createMachineDeploySpec()
+{
+    import std.algorithm : map;
+    import std.array : assocArray;
+    import std.json : JSONValue;
+    import std.typecons : tuple;
+    import std.file : writeFile = write;
+
+    auto params = parseEnv!Params;
+    params.flakePre = "legacyPackages";
+    params.flakePost = ".bareMetalMachines";
+
+    string cachixUrl = "https://" ~ params.cachixCache ~ ".cachix.org";
+    auto packages = nixEvalJobs(params, SupportedSystem.x86_64_linux, cachixUrl);
+
+    auto result = [
+        "agents": packages
+            .map!(pkg => tuple(pkg.name, pkg.output))
+            .assocArray
+    ].JSONValue;
+
+    writeFile(deploySpecFile, result.toString());
 }
