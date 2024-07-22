a604401b09ef67459d579158b6106fb9e1419703
Petar Kirov <petar.p.kirov@gmail.com>
2024-07-22 03:12:08 +0300

    refactor(modules/shard-attrs): Rework the implementation

---
+++ lib/shard-attrs.nix
@@ -0,0 +1,17 @@
+lib: {
+  shardAttrs = attrs: shardSize: let
+    attrNames = builtins.attrNames attrs;
+    shardCount = builtins.ceil ((0.0 + (builtins.length attrNames)) / shardSize);
+    attrNameShards = lib.pipe (lib.range 0 (shardCount - 1)) [
+      (builtins.map (i: lib.sublist (i * shardSize) shardSize attrNames))
+    ];
+    shards = lib.pipe attrNameShards [
+      (lib.imap0 (i: shard: {
+        name = builtins.toString i;
+        value = lib.genAttrs shard (key: attrs.${key});
+      }))
+      lib.listToAttrs
+    ];
+  in
+    shards;
+}
