338bdbf8a88a8c11a6d7674618004f50e9558e09
Petar Kirov <petar.p.kirov@gmail.com>
2024-07-22 03:12:08 +0300

    refactor(modules/shard-attrs): Rework the implementation

--- modules/shard-split/default.nix
+++ modules/shard-split/default.nix
@@ -1,63 +1,32 @@
-{...}: {
-  flake.flakeModules.shardSplit = {
-    inputs,
-    self,
-    config,
-    lib,
-    ...
-  }: {
-    options = {
-      shardSize = with lib;
-        mkOption {
-          type = types.int;
-          default = 1;
-          description = "Number of shards to use for parallel builds";
-        };
+{lib, ...}: let
+  inherit (import ../../lib/shard-attrs.nix lib) shardAttrs;
+in {
+  flake.flakeModules.shardSplit = {config, ...}: let
+    cfg = config.mcl.matrix.shard;
+  in {
+    options.mcl.matrix.shard = with lib; {
+      size = mkOption {
+        type = types.numbers.positive;
+        default = 1;
+        description = "Number of shards to use for parallel builds";
+      };
+      attributePath = mkOption {
+        type = types.listOf types.str;
+        default = ["legacyPackages" "checks"];
+        description = "The attribute path to split into shards";
+      };
     };
+
     config = {
-      perSystem = {
-        pkgs,
-        system,
-        unstablePkgs,
-        inputs',
-        ...
-      }: let
-        shardAttrs = let
-          attrNames = builtins.attrNames self.legacyPackages."${system}".checks;
-          len = builtins.length attrNames;
-          shardCount = builtins.ceil ((0.0 + len) / config.shardSize);
-          shards = builtins.map (i: lib.take config.shardSize (lib.drop (i * config.shardSize) attrNames)) (lib.range 0 (shardCount - 1));
-          shardIndices = builtins.map builtins.toString (lib.range 0 (builtins.length shards));
-        in
-          lib.genAttrs shardIndices (idx: lib.genAttrs (builtins.elemAt shards (builtins.fromJSON idx)) (key: self.legacyPackages."${system}".checks."${key}"));
+      perSystem = {self', ...}: let
+        inherit (cfg) size attributePath;
+        attrs = lib.attrByPath attributePath {} self';
+        shards = shardAttrs attrs size;
       in {
-        legacyPackages = let
-          _shards = shardAttrs;
-          _shardCount = builtins.length (builtins.attrNames _shards) - 1;
-          shardCount =
-            if !pkgs.hostPlatform.isLinux
-            then self.legacyPackages.x86_64-linux.shardCount
-            else _shardCount;
-          shards =
-            if !pkgs.hostPlatform.isLinux
-            then let
-              rShardCount = self.legacyPackages.x86_64-linux.shardCount;
-            in
-              builtins.listToAttrs (
-                builtins.map (i:
-                  if (i < _shardCount)
-                  then {
-                    name = "${builtins.toString i}";
-                    value = _shards."${builtins.toString i}";
-                  }
-                  else {
-                    name = "${builtins.toString i}";
-                    value = {};
-                  }) (lib.range 0 rShardCount)
-              )
-            else _shards;
-        in {
-          inherit shards shardCount;
+        legacyPackages.mcl.matrix = {
+          shardCount = builtins.length (builtins.attrValues shards);
+          shardSize = cfg.size;
+          inherit shards attributePath;
         };
       };
     };
