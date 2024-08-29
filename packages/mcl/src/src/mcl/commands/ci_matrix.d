d5a95d6d802860ace7eb1be02d53bc29bfa907e1
Simeon Armenchev <monyarm@gmail.com>
2024-08-29 05:44:45 +0300

    fix(mcl/ci_matrix): Add warnings to uselessWarnings

--- packages/mcl/src/src/mcl/commands/ci_matrix.d
+++ packages/mcl/src/src/mcl/commands/ci_matrix.d
@@ -238,7 +238,13 @@ static immutable string[] uselessWarnings =
         "Please note that certain features, such as the test framework, may not function properly with the legacy table type.",
         "If you encounter errors similar to:",
         "error: The option `disko.devices.disk.disk1.content.partitions",
-        "this is likely due to the use of the legacy table type."
+        "this is likely due to the use of the legacy table type.",
+        "warning: system.stateVersion is not set, defaulting to",
+        "warning: Runner registration tokens have been deprecated and disabled by default in GitLab >= 17.0.",
+        "Consider migrating to runner authentication tokens by setting `services.gitlab-runner.services.codetracer.authenticationTokenConfigFile`.",
+        "https://docs.gitlab.com/17.0/ee/ci/runners/new_creation_workflow.html"
+        "for a migration you can follow the guide at https://github.com/nix-community/disko/blob/master/docs/table-to-gpt.md"
+
     ];
 
 Package packageFromNixEvalJobsJson(
@@ -371,7 +377,7 @@ Package[] nixEvalJobs(string flakeAttrPrefix, string cachixUrl, bool doCheck = t
     }
     foreach (line; pipes.stderr.byLine)
     {
-        if (uselessWarnings.map!((warning) => line.indexOf(warning) != -1).any)
+        if (line.trim == "" || uselessWarnings.map!((warning) => line.indexOf(warning) != -1).any)
             continue;
 
         logError(line.idup);
