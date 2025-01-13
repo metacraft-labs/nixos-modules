40dac524a3cb74728851c0f9ea27ffb558219184
Simeon Armenchev <monyarm@gmail.com>
2025-01-13 08:26:45 +0200

    feat(mcl): Add commandline argument support

--- packages/mcl/src/src/mcl/commands/package.d
+++ packages/mcl/src/src/mcl/commands/package.d
@@ -6,4 +6,4 @@ public import mcl.commands.ci_matrix : ci_matrix, print_table;
 public import mcl.commands.shard_matrix : shard_matrix;
 public import mcl.commands.ci : ci;
 public import mcl.commands.host_info : host_info;
-public import mcl.commands.machine_create : machine_create;
+public import mcl.commands.machine : machine;
