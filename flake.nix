dbc4d3bc213d7b2476c20f7a0c90ec8a9654e17e
Petar Kirov <petar.p.kirov@gmail.com>
2025-02-21 12:54:01 +0200

    fix(flake/systems): Temporary remove systems != CI.system to workaround use of IFG in ethereum.nix/nimbus-eth2

--- flake.nix
+++ flake.nix
@@ -226,9 +226,6 @@
       ];
       systems = [
         "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
       perSystem =
         { system, ... }:
