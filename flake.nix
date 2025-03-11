e2497efeba613697b2019b52f6d5e1b1e4ce6228
Galin Stefanov <galinstef10@gmail.com>
2025-03-11 17:43:45 +0200

    config(flake.nix): Add aarch64-darwin aarch64-linux and x86_64-darwin to systems

--- flake.nix
+++ flake.nix
@@ -225,6 +225,9 @@
       ];
       systems = [
         "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
       perSystem =
         { system, ... }:
