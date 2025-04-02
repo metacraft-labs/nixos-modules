97dc4a301639418640faf1f8d0aae3109b97f6cf
Simeon Armenchev <monyarm@gmail.com>
2025-04-02 15:16:01 +0300

    feat(commands/config): Enhance config command with argparse integration and structured argument handling

--- packages/mcl/src/src/mcl/commands/package.d
+++ packages/mcl/src/src/mcl/commands/package.d
@@ -7,4 +7,4 @@ public import mcl.commands.shard_matrix : shard_matrix;
 public import mcl.commands.ci : ci;
 public import mcl.commands.host_info : host_info, host_info_args;
 public import mcl.commands.machine : machine;
-public import mcl.commands.config : config;
+public import mcl.commands.config : config, config_args;
