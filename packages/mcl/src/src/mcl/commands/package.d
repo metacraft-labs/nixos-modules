f0c7b3acc55b26cf3deb2a1050d80a4b30c9507f
Simeon Armenchev <monyarm@gmail.com>
2025-04-02 15:15:21 +0300

    feat(commands/host_info): Enhance host_info command with argparse integration and structured argument handling

--- packages/mcl/src/src/mcl/commands/package.d
+++ packages/mcl/src/src/mcl/commands/package.d
@@ -5,6 +5,6 @@ public import mcl.commands.deploy_spec : deploy_spec, deploy_spec_args;
 public import mcl.commands.ci_matrix : ci_matrix, print_table;
 public import mcl.commands.shard_matrix : shard_matrix;
 public import mcl.commands.ci : ci;
-public import mcl.commands.host_info : host_info;
+public import mcl.commands.host_info : host_info, host_info_args;
 public import mcl.commands.machine : machine;
 public import mcl.commands.config : config;
