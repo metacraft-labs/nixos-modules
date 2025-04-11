830750ec46576f70bcf59908bd2cbb575a485570
Simeon Armenchev <monyarm@gmail.com>
2025-04-11 12:27:06 +0300

    feat(mcl): Add command aliases for backwards compatibility

--- packages/mcl/src/src/mcl/commands/shard_matrix.d
+++ packages/mcl/src/src/mcl/commands/shard_matrix.d
@@ -20,7 +20,7 @@ import mcl.utils.json : toJSON;
 import mcl.utils.nix : nix;
 import mcl.utils.path : createResultDirs, resultDir, rootDir;
 
-@(Command("shard-matrix").Description("Generate a shard matrix for a flake"))
+@(Command("shard-matrix", "shard_matrix").Description("Generate a shard matrix for a flake"))
 struct shard_matrix_args
 {
     @(NamedArgument(["github-output"]).Placeholder("output").Description("Output to GitHub Actions"))
