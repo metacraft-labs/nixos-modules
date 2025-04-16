43f3618428acabf94b80462a274f3e3da58db010
Galin Stefanov <galinstef10@gmail.com>
2025-04-16 13:16:00 +0300

    config(commit_flake_update.bash): Add FLAKE_INPUT as a parameter with empty default value

--- scripts/commit_flake_update.bash
+++ scripts/commit_flake_update.bash
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+FLAKE_INPUT=${FLAKE_INPUT:-""}
+
 if ! git config --get user.name >/dev/null 2>&1 || \
   [ "$(git config --get user.name)" = "" ] ||
   ! git config --get user.email >/dev/null 2>&1 || \
@@ -15,11 +17,15 @@ fi
 current_commit="$(git rev-parse HEAD)"
 export PRE_COMMIT_ALLOW_NO_CONFIG=1
 
-nix flake update --accept-flake-config --commit-lock-file
+nix flake update $FLAKE_INPUT --accept-flake-config --commit-lock-file
 commit_after_update="$(git rev-parse HEAD)"
 
 if [[ "$commit_after_update" = "$current_commit" ]]; then
-  echo "All flake inputs are up to date."
+  if [[ "$FLAKE_INPUT" = "" ]]; then
+    echo "All flake inputs are up to date."
+  else
+    echo "$FLAKE_INPUT input is up to date."
+  fi
   exit 0
 fi
 
@@ -30,11 +36,19 @@ msg_file=./commit_msg_body.txt
   echo '```'
 } > $msg_file
 
-git commit --amend -F - <<EOF
-chore(flake.lock): Update all Flake inputs ($(date -I))
+if [[ "$FLAKE_INPUT" = "" ]]; then
+  git commit --amend -F - <<EOF
+  chore(flake.lock): Update all Flake inputs ($(date -I))
 
-$(cat $msg_file)
+  $(cat $msg_file)
 EOF
+else
+  git commit --amend -F - <<EOF
+  chore(flake.lock): Update \`$FLAKE_INPUT\` Flake input ($(date -I))
+
+  $(cat $msg_file)
+EOF
+fi
 
 if [ -z "${CI+x}" ]; then
   rm -v $msg_file
