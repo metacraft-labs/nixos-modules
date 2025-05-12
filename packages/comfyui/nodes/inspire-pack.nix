667ad4442a1067e929808a76074b0be25a315235
monyarm <monyarm@gmail.com>
2025-05-12 18:12:05 +0300

    feat(pkgs): Add Inspire node pack for ComfyUI with necessary dependencies

---
+++ packages/comfyui/nodes/inspire-pack.nix
@@ -0,0 +1,28 @@
+{
+  stdenv,
+  python312,
+  fetchFromGitHub,
+  lib,
+}:
+stdenv.mkDerivation rec {
+  pname = "inspire-pack";
+  version = "1.18";
+  src = fetchFromGitHub {
+    owner = "ltdrdata";
+    repo = "ComfyUI-Inspire-Pack";
+    rev = version;
+    hash = "sha256-mRE6RWK4gwc6Hw1gL2xeIWpSzT677QnBTNAy9OdutLM=";
+  };
+  installPhase = ''
+    mkdir -p $out/custom_nodes/${pname}
+    cp -r $src/* $out/custom_nodes/${pname}
+  '';
+
+  dependencies = with python312.pkgs; [
+    matplotlib
+    cachetools
+    numpy_1
+    webcolors
+    opencv-python-headless
+  ];
+}
