3cf610d76c5ba92f8867f91181bf77868ef16f85
monyarm <monyarm@gmail.com>
2025-05-12 18:05:05 +0300

    feat(pkgs): Add new Python package pilgram version unstable

---
+++ packages/python-packages/pilgram/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchgit,
+  setuptools,
+  python312,
+  poetry,
+}:
+buildPythonPackage rec {
+  pname = "pilgram";
+  version = "unstable";
+  pyproject = true;
+
+  nativeBuildInputs = buildInputs;
+  preBuild = ''
+    export HOME=$TMPDIR
+  '';
+  buildInputs = with python312.pkgs; [
+    numpy
+    pillow
+    poetry
+    poetry-core
+    flake8
+    black
+    isort
+    pytest
+    pytest-cov
+    pytest-mock
+    pytest-benchmark
+  ];
+
+  src = fetchgit {
+    url = "https://github.com/akiomik/pilgram";
+    rev = "ecef609b233b2f650dd82ca90d3c3d20148849c7";
+    hash = "sha256-j8mqUwMZdyN7XdvJ141GTVyW9XtNN5bQFCOscM7NqbA=";
+  };
+
+  meta = {
+    description = "";
+    homepage = "https://github.com/akiomik/pilgram";
+  };
+}
