1ce7a753b6038e54bb4002bd3fc2467120d6d8ef
monyarm <monyarm@gmail.com>
2025-05-12 18:07:11 +0300

    feat(pkgs): Add new Python package transparent-background version unstable

---
+++ packages/python-packages/transparent-background/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchgit,
+  setuptools,
+  python312,
+}:
+buildPythonPackage rec {
+  pname = "transparent-background";
+  version = "unstable";
+
+  buildInputs = with python312.pkgs; [
+    numpy
+    opencv-python-headless
+    pillow
+  ];
+
+  preBuild = ''
+    export HOME=$TMPDIR
+  '';
+  preInstall = ''
+    ls $HOME
+  '';
+
+  src = fetchgit {
+    url = "https://github.com/plemeri/transparent-background";
+    rev = "60c6bc4b3d326d592f6e99568a5fdc5a28d9d791";
+    hash = "sha256-MSvq7WX9gb9+UmvAh654OMXcCZIVrErL/rKtmjXnT0U=";
+  };
+
+  meta = {
+    description = "";
+    homepage = "https://github.com/plemeri/transparent-background";
+  };
+}
