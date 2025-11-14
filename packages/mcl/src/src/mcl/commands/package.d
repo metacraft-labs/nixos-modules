669ed8b10981c045a51d63fd194252a50f05aea4
Petar Kirov <petar.p.kirov@gmail.com>
2025-11-14 17:23:07 +0200

    refactor(mcl.commands): Improve meta-programming to automatically define top-level commands

--- packages/mcl/src/src/mcl/commands/package.d
+++ packages/mcl/src/src/mcl/commands/package.d
@@ -1,10 +1,26 @@
 module mcl.commands;
 
-public import mcl.commands.get_fstab : get_fstab, get_fstab_args;
-public import mcl.commands.deploy_spec : deploy_spec, deploy_spec_args;
-public import mcl.commands.ci_matrix : ci_matrix,ci_matrix_args, print_table,print_table_args;
-public import mcl.commands.shard_matrix : shard_matrix, shard_matrix_args;
-public import mcl.commands.ci : ci, ci_args;
-public import mcl.commands.host_info : host_info, host_info_args;
-public import mcl.commands.machine : machine, machine_args;
-public import mcl.commands.config : config, config_args;
+import std.meta : ApplyLeft, staticMap;
+
+private enum commandModulesToExport =
+[
+    "mcl.commands.deploy_spec" : ["deploy_spec"],
+    "mcl.commands.ci_matrix" : ["ci_matrix", "print_table"],
+    "mcl.commands.shard_matrix" : ["shard_matrix"],
+    "mcl.commands.ci" : ["ci"],
+    "mcl.commands.host_info" : ["host_info"],
+    "mcl.commands.machine" : ["machine"],
+    "mcl.commands.config" : ["config"],
+];
+
+template ImportAll(alias aa)
+{
+    import std.meta : AliasSeq;
+    alias A = AliasSeq!();
+    static foreach (moduleName, symbols; aa)
+        static foreach (symbolName; symbols)
+            A = AliasSeq!(A, __traits(getMember, imported!moduleName, symbolName));
+    alias ImportAll = A;
+}
+
+alias SubCommandFunctions = ImportAll!commandModulesToExport;
